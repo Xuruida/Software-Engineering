e35921788be53b644e4a5dcc4d4ab9f3c565e6a7
XuRuida <2475503376@qq.com>
2020-03-12 01:54:05 +0800

    feat(lab3): implement basic functions without keyboard event

--- projects/lab3/timer/src/App.vue
+++ projects/lab3/timer/src/App.vue
@@ -2,7 +2,19 @@
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
     <timer-header
-      @update-time="updateLimitTime">
+      @update-time="updateLimitTime"
+      @start-down-timing="startDownTiming"
+      @start-up-timing="startUpTiming"
+      @pause-timing="pauseTiming"
+      @resume-timing="resumeTiming"
+      @restart-timing="restartTiming"
+      @clear-timing="clearTiming"
+      :limit-str="limitStr"
+      :is-started="isStarted"
+      :is-finished="isFinished"
+      :is-down-timing="isDownTiming"
+      :is-paused="isPaused"
+      :is-changed-display="isChangedDisplay">
     </timer-header>
     <!-- <router-view/> -->
     <timer-body :clock-str="clockStr">
@@ -18,19 +30,21 @@ export default {
   name: 'App',
   data: function () {
     return {
+      limitStr: '',
       limitHour: 0,
       limitMinute: 0,
       limitSecond: 0,
-      hour: 23,
-      minute: 59,
-      second: 59
-    }
-  },
-  watch: {
-    hour: function (val) {
-      if (val === 1) {
-        this.$emit('cao', 'aaaooee')
-      }
+      hour: 0,
+      minute: 0,
+      second: 0,
+      remainningTime: 0,
+      limitTime: 0,
+      ms: 0,
+      isStarted: false,
+      isFinished: false,
+      isDownTiming: true,
+      isPaused: false,
+      isChangedDisplay: false
     }
   },
   computed: {
@@ -52,6 +66,39 @@ export default {
   },
   methods: {
     updateLimitTime: function (inputHour, inputMinute, inputSecond) {
+      this.getInputInfo(inputHour, inputMinute, inputSecond)
+      this.displayAttr[2] = false
+      this.isChangedDisplay = !this.isChangedDisplay
+      console.log('changed', this.change)
+      this.updateTime()
+    },
+
+    updateTime: function () {
+      const timeNow = Date.now()
+      this.remainningTime = this.finishTime - timeNow
+      const currentTime = this.limitTime - this.remainningTime
+      if (this.remainningTime < 0) {
+        clearInterval(timeObj)
+        timeObj = null
+        this.remainningTime = 0
+        this.changeDisplay(true, true, this.isDownTiming, false)
+      }
+      if (this.isDownTiming) {
+        const tmpArr = calcHMS(this.remainningTime)
+        this.hour = tmpArr[0]
+        this.minute = tmpArr[1]
+        this.second = tmpArr[2]
+        this.ms = tmpArr[3]
+      } else {
+        const tmpArr = calcHMS(currentTime)
+        this.hour = tmpArr[0]
+        this.minute = tmpArr[1]
+        this.second = tmpArr[2]
+        this.ms = tmpArr[3]
+      }
+    },
+
+    getInputInfo: function (inputHour, inputMinute, inputSecond) {
       if (isNaN(inputHour)) inputHour = 0
       if (inputHour > 99) inputHour = 99
       else if (inputHour < 0) inputHour = 0
@@ -67,14 +114,110 @@ export default {
       else if (inputSecond < 0) inputSecond = 0
       this.limitSecond = inputSecond
 
-      this.updateTime()
+      let strHour = this.limitHour.toString()
+      let strMinute = this.limitMinute.toString()
+      let strSecond = this.limitSecond.toString()
+      if (strHour < 10) strHour = '0' + strHour
+      if (strMinute < 10) strMinute = '0' + strMinute
+      if (strSecond < 10) strSecond = '0' + strSecond
+      this.limitStr = strHour + ':' + strMinute + ':' + strSecond
+
+      this.ms = 0
+      this.remainningTime = this.limitTime =
+        calcMs(this.limitHour, this.limitMinute, this.limitSecond, this.ms)
+
+      this.finishTime = Date.now() + this.limitTime
+      console.log(Date.now(), this.finishTime, this.limitTime)
     },
-    updateTime: function () {
-      this.hour = this.limitHour
-      this.minute = this.limitMinute
-      this.second = this.limitSecond
+
+    changeDisplay: function (boSt, boFi, boDo, boPa) {
+      this.isStarted = boSt
+      this.isFinished = boFi
+      this.isDownTiming = boDo
+      this.isPaused = boPa
+      this.isChangedDisplay = !this.isChangedDisplay
+    },
+
+    startDownTiming: function (inputHour, inputMinute, inputSecond) {
+      // initialize
+      if (timeObj === null) {
+        this.changeDisplay(true, false, true, false)
+        this.getInputInfo(inputHour, inputMinute, inputSecond)
+        setTimeout(() => {
+          this.updateTime()
+          timeObj = setInterval(this.updateTime, INTERVAL)
+        }, 1)
+      }
+    },
+
+    startUpTiming: function (inputHour, inputMinute, inputSecond) {
+      // initialize
+      if (timeObj === null) {
+        this.changeDisplay(true, false, false, false)
+        this.getInputInfo(inputHour, inputMinute, inputSecond)
+        setTimeout(() => {
+          this.updateTime()
+          timeObj = setInterval(this.updateTime, INTERVAL)
+        }, 1)
+      }
+    },
+
+    pauseTiming: function () {
+      if (timeObj !== null) {
+        this.changeDisplay(true, false, this.isDownTiming, true)
+        clearInterval(timeObj)
+        timeObj = null
+      }
+    },
+
+    resumeTiming: function () {
+      if (timeObj === null) {
+        this.changeDisplay(true, false, this.isDownTiming, false)
+        this.finishTime = this.remainningTime + Date.now()
+        timeObj = setInterval(this.updateTime, INTERVAL)
+      }
+    },
+
+    restartTiming: function () {
+      if (timeObj !== null) {
+        clearInterval(timeObj)
+        timeObj = null
+      }
+      this.changeDisplay(true, false, this.isDownTiming, false)
+      this.finishTime = this.limitTime + Date.now()
+      setTimeout(() => {
+        this.updateTime()
+        timeObj = setInterval(this.updateTime, INTERVAL)
+      }, 1)
+    },
+
+    clearTiming: function () {
+      if (timeObj !== null) {
+        clearInterval(timeObj)
+        timeObj = null
+      }
+      this.changeDisplay(false, false, true, false)
+      this.hour = this.minute = this.second = this.ms = 0
     }
   }
+}
+let timeObj = null
+const INTERVAL = 50
+function calcMs (hr, min, sec, ms) {
+  const tmpTime = 1000 * (60 * (60 * hr + min) + sec) + ms
+  return tmpTime
+}
+
+function calcHMS (tmpTime) {
+  const arr = new Array(4)
+  arr[3] = tmpTime % 1000
+  tmpTime = parseInt(tmpTime / 1000)
+  arr[2] = tmpTime % 60
+  tmpTime = parseInt(tmpTime / 60)
+  arr[1] = tmpTime % 60
+  arr[0] = parseInt(tmpTime / 60)
+  console.log(arr)
+  return arr
 }
 </script>
 
--- projects/lab3/timer/src/components/TimerHeader.vue
+++ projects/lab3/timer/src/components/TimerHeader.vue
@@ -2,19 +2,21 @@
   <div class="head">
     <div class="empty_blk"></div>
     <timer-input
-      v-show="isTiming === false"
+      v-show="disAttr[0]"
       v-for="item in items"
       :key="item.id"
       :input-attr="item.inputAttr"
       :show-text="item.showText"
       :ref="item.inputAttr">
     </timer-input>
-    <timer-button color="blue" @click="updateTime" v-show="isTiming === false">开始正计时 </timer-button>
-    <timer-button color="blue" @click="updateTime" v-show="isTiming === false">开始倒计时 </timer-button>
-    <p class="info_text" id="hint" v-show="isTiming === true">test</p>
-    <timer-button color="blue" @click="updateTime" v-show="isTiming === true">暂停计时器</timer-button>
-    <timer-button color="yellow" position="right" v-show="isTiming === true">重新再计时</timer-button>
-    <timer-button color="red" position="right" v-show="isTiming === true">清空计时器</timer-button>
+    <timer-button color="blue" @click="startUpTiming" id="countup" v-show="disAttr[1] === true">开始正计时 </timer-button>
+    <timer-button color="blue" @click="startDownTiming" id="countdown" v-show="disAttr[2] === true">开始倒计时 </timer-button>
+    <p class="info_text" id="hint" v-show="disAttr[3] === true">{{ hintText }}</p>
+    <timer-button color="blue" @click="pauseTiming" id="pause" v-show="disAttr[4] === true">暂停计时器</timer-button>
+    <timer-button color="blue" @click="resumeTiming" id="resume" v-show="disAttr[5] === true">恢复计时器</timer-button>
+    <div class="empty_blk_right"></div>
+    <timer-button color="yellow" position="right" @click="restartTiming" id="restart" v-show="disAttr[6] === true">重新再计时</timer-button>
+    <timer-button color="red" position="right" @click="clearTiming" id="clear" v-show="disAttr[7] === true">清空计时器</timer-button>
   </div>
 </template>
 
@@ -25,7 +27,9 @@ export default {
   name: 'TimerHeader',
   data: function () {
     return {
+      disAttr: [true, true, true, false, false, false, false, false],
       test: 0,
+      hintText: '',
       isTiming: false,
       items: [
         {
@@ -43,12 +47,93 @@ export default {
           inputAttr: 'second',
           showText: '秒'
         }
+      ],
+      constDisAttr: [
+        // !isStarted
+        {
+          boArr: [true, true, true, false, false, false, false, false],
+          hintStr: ''
+        },
+        // isStarted && !isFinished
+        { // isDownTiming, !isPaused
+          boArr: [false, false, false, true, true, false, true, true],
+          hintStr: '正在倒计时 '
+        },
+        { // isDownTiming, isPaused
+          boArr: [false, false, false, true, false, true, true, true],
+          hintStr: '暂停倒计时 '
+        },
+        { // !isDownTiming, !isPaused
+          boArr: [false, false, false, true, true, false, true, true],
+          hintStr: '正在正计时 '
+        },
+        { // !isDownTiming, isPaused
+          boArr: [false, false, false, true, false, true, true, true],
+          hintStr: '暂停正计时 '
+        },
+        // isFinished
+        {
+          // isDownTiming
+          boArr: [false, false, false, true, false, false, true, true],
+          hintStr: ['倒计时', '已结束']
+        },
+        {
+          // !isDownTiming
+          boArr: [false, false, false, true, false, false, true, true],
+          hintStr: ['正计时', '已结束']
+        }
       ]
     }
   },
+  props: {
+    isChangedDisplay: {
+      type: Boolean,
+      default: false
+    },
+    isStarted: {
+      type: Boolean,
+      default: false
+    },
+    isFinished: {
+      type: Boolean,
+      default: false
+    },
+    isDownTiming: Boolean,
+    isPaused: Boolean,
+    limitStr: {
+      type: String,
+      default: ''
+    }
+  },
+  watch: {
+    isChangedDisplay: {
+      handler: function (val, oldVal) {
+        console.log(this.isStarted, this.isFinished, this.isDownTiming, this.isPaused)
+        if (!this.isStarted) {
+          this.changeDisplay(0)
+        } else {
+          if (!this.isFinished) {
+            if (this.isDownTiming) {
+              if (!this.isPaused) this.changeDisplay(1)
+              else this.changeDisplay(2)
+            } else {
+              if (!this.isPaused) this.changeDisplay(3)
+              else this.changeDisplay(4)
+            }
+          } else {
+            if (this.isDownTiming) this.changeDisplay(5)
+            else this.changeDisplay(6)
+          }
+        }
+      }
+    }
+  },
+  components: {
+    TimerButton,
+    TimerInput
+  },
   methods: {
     updateTime: function () {
-      this.$on('cao', 'alert("I did it.")')
       this.$emit(
         'update-time',
         Number(this.$refs.hour[0].test),
@@ -59,11 +144,44 @@ export default {
         this.$refs.hour[0].test,
         this.$refs.minute[0].test,
         this.$refs.second[0].test)
+    },
+    emitTiming: function (tmpStr) {
+      this.$emit(
+        tmpStr,
+        Number(this.$refs.hour[0].test),
+        Number(this.$refs.minute[0].test),
+        Number(this.$refs.second[0].test))
+      console.log(
+        tmpStr,
+        this.$refs.hour[0].test,
+        this.$refs.minute[0].test,
+        this.$refs.second[0].test)
+    },
+    startDownTiming: function () {
+      this.emitTiming('start-down-timing')
+    },
+    startUpTiming: function () {
+      this.emitTiming('start-up-timing')
+    },
+    pauseTiming: function () {
+      this.$emit('pause-timing')
+    },
+    resumeTiming: function () {
+      this.$emit('resume-timing')
+    },
+    restartTiming: function () {
+      this.$emit('restart-timing')
+    },
+    clearTiming: function () {
+      this.$emit('clear-timing')
+    },
+    changeDisplay: function (idx) {
+      let tmpObj = this.constDisAttr[idx]
+      if (idx < 5) this.hintText = tmpObj.hintStr + this.limitStr
+      else this.hintText = [tmpObj.hintStr[0], this.limitStr, tmpObj.hintStr[1]].join(' ')
+      this.disAttr = this.constDisAttr[idx].boArr
+      console.log(this.hintText, this.disAttr, this.limitStr)
     }
-  },
-  components: {
-    TimerButton,
-    TimerInput
   }
 }
 </script>
@@ -81,8 +199,8 @@ export default {
 
 .info_text {
   color: #fff;
-  display: none;
   font-family: PT Mono, PingFang SC, sans-serif;
+  display: inline-block;
   font-size: 16px;
   margin: 24px 10px;
 }
